789c3b0e36732b22a392a21c38bf34c2122f69c7
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-20 20:21:37 +0100

    feat(ToDo): printUsage function created!

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -5,6 +5,19 @@ Then it should print the usage instructions
  */
 public class ToDo {
   public static void main(String[] args) {
+    if(args.length == 0){
+      printUsage();
+    }
+  }
 
+  private static void printUsage() {
+    System.out.print("\nCommand Line Todo application\n");
+    System.out.print("=============================\n\n");
+
+    System.out.println("Command line arguments:");
+    System.out.println("    -l   Lists all the tasks");
+    System.out.println("    -a   Adds a new task");
+    System.out.println("    -r   Removes a task");
+    System.out.println("    -c   Completes a task");
   }
 }
