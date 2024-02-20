cce2194cecc3eeeb33f9b014ea3800c3d198d888
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-20 21:30:51 +0100

    feat(ToDo): listTasks method and todos.txt added!
    New feature: a .txt file added to store todos
    New feature: can list all todos with the -l argument upon running the program

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -1,4 +1,10 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.nio.*;
+import java.util.List;
 
 /*
 Given the terminal opened in the project directory
@@ -14,8 +20,7 @@ public class ToDo {
 
     switch (args[0]){
       case "-l":
-        //not implemented yet
-        System.out.println("Function is not implemented yet");
+        listTasks();
         break;
       case "-a":
         //not implemented yet
@@ -36,6 +41,8 @@ public class ToDo {
     }
   }
 
+
+
   private static void printUsage() {
     System.out.print("\nCommand Line Todo application\n");
     System.out.print("=============================\n\n");
@@ -45,5 +52,33 @@ public class ToDo {
     System.out.println("    -a   Adds a new task");
     System.out.println("    -r   Removes a task");
     System.out.println("    -c   Completes a task");
+  }
+
+  /**
+   * Given the terminal opened in the project directory
+   * And the file where you store your data exists
+   * And a task with the description Walk the dog is stored in the file
+   * And a task with the description Buy milk is stored in the file
+   * And a task with the description Do homework is stored in the file
+   * When the application is run with -l argument
+   * Then it should print the tasks that are stored in the file
+   * And it should add numbers before each task
+   *
+   * $ todo -l
+   *
+   * 1 - Walk the dog
+   * 2 - Buy milk
+   * 3 - Do homework
+   */
+  private static void listTasks() {
+    List<String> toDoList;
+    try {
+      toDoList=Files.readAllLines(Paths.get("todos.txt"));
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    for (int i = 0; i < toDoList.size(); i++) {
+      System.out.printf("%d - %s\n",i+1,toDoList.get(i));
+    }
   }
 }
