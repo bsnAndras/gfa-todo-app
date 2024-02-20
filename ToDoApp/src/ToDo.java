5a738f59ab3c32147f026da022c2a96d160ca70a
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-20 20:14:28 +0100

    chore(ToDo): main method added

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -1,3 +1,10 @@
+/*
+Given the terminal opened in the project directory
+When the application is run without any arguments
+Then it should print the usage instructions
+ */
 public class ToDo {
+  public static void main(String[] args) {
 
+  }
 }
