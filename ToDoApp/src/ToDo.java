f30530e4a68097d8816ab50fedf6fc5a59288d15
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-22 23:05:37 +0100

    style(ToDo): descriptions added

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -7,13 +7,8 @@ public class ToDo {
   public static final Path FILEPATH = Paths.get(
       "C:\\Users\\F. András\\Documents\\Prog\\Workspace\\bsnAndras-todo-app\\ToDoApp\\src\\todos.txt");
   public static List<Task> toDoList = new ArrayList<>();
-
   public static void main(String[] args) {
-    /*
-    Given the terminal opened in the project directory
-    When the application is run without any arguments
-    Then it should print the usage instructions
-    */
+
     if (args.length == 0) {
       printUsage();
       return;
@@ -26,7 +21,7 @@ public class ToDo {
         break;
       case "-a":
         //not implemented yet
-        addTask(args[1],FILEPATH);
+        addTask(args[1], FILEPATH);
         break;
       case "-r":
         //not implemented yet
@@ -43,7 +38,14 @@ public class ToDo {
     }
   }
 
-
+  /**
+   * <h1>Print usage</h1>
+   *<ul>
+   *<li><b>Given</b> the terminal opened in the project directory</li>
+   *<li><b>When</b> the application is run without any arguments</li>
+   *<li><b>Then</b> it should print the usage instructions</li>
+   *</ul>
+   */
   private static void printUsage() {
     System.out.print("\nCommand Line Todo application\n");
     System.out.print("=============================\n\n");
@@ -55,26 +57,30 @@ public class ToDo {
     System.out.println("    -c   Completes a task");
   }
 
-  /**
-   * Given the terminal opened in the project directory
-   * And the file where you store your data exists
-   * And a task with the description Walk the dog is stored in the file
-   * And a task with the description Buy milk is stored in the file
-   * And a task with the description Do homework is stored in the file
-   * When the application is run with -l argument
-   * Then it should print the tasks that are stored in the file
-   * And it should add numbers before each task
+  /**<h1>List tasks</h1>
+   * <ul>
+   *  <ul>
+   *   <li><b>Given</b> the terminal opened in the project directory</li>
+   * <li>And the file where you store your data exists</li>
+   * <li>And a task with the description Walk the dog is stored in the file</li>
+   * <li>And a task with the description Buy milk is stored in the file</li>
+   * <li>And a task with the description Do homework is stored in the file</li>
+   * </ul>
+   * <ul><li><b>When</b> the application is run with -l argument</li></ul>
+   * <ul><li><b>Then</b> it should print the tasks that are stored in the file</li>
+   * <li>And it should add numbers before each task</li></ul>
+   * </ul>
    * $ todo -l
    * 1 - Walk the dog
    * 2 - Buy milk
    * 3 - Do homework
    * <p>
-   * ### Empty list:
-   * - **Given** the terminal opened in the project directory
+   * <h2>Empty list</h2>
+   * - <b>Given</b> the terminal opened in the project directory
    * - And the file where you store your data exists
    * - And the file has 0 task
-   * - **When** the application is ran with `-l` argument
-   * - **Then** it should show a message like this: `No todos for today! :)`
+   * - <b>When</b> the application is ran with `-l` argument
+   * - <b>Then</b> it should show a message like this: `No todos for today! :)`
    */
   private static void listTasks(Path filePath) {
     toDoList = loadToDos(filePath);
@@ -109,21 +115,39 @@ public class ToDo {
     }
     return fileContent;
   }
-  public static int writeFile(String newLine,Path filePath) {
+
+  public static int writeFile(String newLine, Path filePath) {
 
     try {
-      Files.writeString(filePath,newLine,StandardOpenOption.CREATE,StandardOpenOption.APPEND);
+      Files.writeString(filePath, newLine, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
     return 0;
   }
 
-  public static int addTask(String newTaskDescription,Path filePath) {
+  /**
+   * <h1>Add new task</h1>
+   * <ul>
+   *   <li><b>Given</b> the terminal opened in the project directory</li>
+   *   <li><b>When</b> the application is ran with the `-a "Feed the monkey"` argument</li>
+   *   <li><b>Then</b> it should add a new task with the description *Feed the monkey*</li>
+   * </ul>
+   * <h2>Add new task error handling</h2>
+   *  <ul>
+   *    <li><b>Given</b> the terminal opened in the project directory</li>
+   *    <li><b>When</b> the application is ran with the <b>-a</b> argument</li>
+   *    <li><b>Then</b> it should show an error message like:</li>
+   *    <li><em>`Unable to add: no task provided`</em></li>
+   *  </ul>
+   */
+  public static int addTask(String newTaskDescription, Path filePath) {
     Task newTask = new Task(newTaskDescription);
-    toDoList = loadToDos(filePath);
+    if (Files.exists(filePath)) {
+      toDoList = loadToDos(filePath);
+    }
     toDoList.add(newTask);
-    writeFile(newTaskDescription,filePath);
+    writeFile(newTaskDescription, filePath);
     return 0;
   }
 }
