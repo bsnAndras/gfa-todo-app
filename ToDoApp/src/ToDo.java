7d6ad04260765b74cf46cdec60eeb5382e0d17cc
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-22 23:08:12 +0100

    feat(ToDo): addTask error handling

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -7,6 +7,7 @@ public class ToDo {
   public static final Path FILEPATH = Paths.get(
       "C:\\Users\\F. András\\Documents\\Prog\\Workspace\\bsnAndras-todo-app\\ToDoApp\\src\\todos.txt");
   public static List<Task> toDoList = new ArrayList<>();
+
   public static void main(String[] args) {
 
     if (args.length == 0) {
@@ -20,8 +21,12 @@ public class ToDo {
         listTasks(FILEPATH);
         break;
       case "-a":
-        //not implemented yet
-        addTask(args[1], FILEPATH);
+        //for adding tasks
+        if (args.length >= 2) {
+          addTask(args[1], FILEPATH);
+        } else {
+          System.err.println("Unable to add: no task provided");
+        }
         break;
       case "-r":
         //not implemented yet
@@ -40,11 +45,11 @@ public class ToDo {
 
   /**
    * <h1>Print usage</h1>
-   *<ul>
-   *<li><b>Given</b> the terminal opened in the project directory</li>
-   *<li><b>When</b> the application is run without any arguments</li>
-   *<li><b>Then</b> it should print the usage instructions</li>
-   *</ul>
+   * <ul>
+   * <li><b>Given</b> the terminal opened in the project directory</li>
+   * <li><b>When</b> the application is run without any arguments</li>
+   * <li><b>Then</b> it should print the usage instructions</li>
+   * </ul>
    */
   private static void printUsage() {
     System.out.print("\nCommand Line Todo application\n");
@@ -57,7 +62,8 @@ public class ToDo {
     System.out.println("    -c   Completes a task");
   }
 
-  /**<h1>List tasks</h1>
+  /**
+   * <h1>List tasks</h1>
    * <ul>
    *  <ul>
    *   <li><b>Given</b> the terminal opened in the project directory</li>
