56247d96cd8b159cdb2e81e5bc027d5898d8b6ab
Andras Foldesi <fagottos.foldesi.andras@gmail.com>
2024-02-24 17:34:24 +0100

    chore(ToDo.Task): updated State enum with values

--- ToDoApp/src/Task.java
+++ ToDoApp/src/Task.java
@@ -1,12 +1,24 @@
 public class Task {
+  //[state] <description>
   String description;
   State state;
 
-  enum State{TODO,IN_PROGRESS,DONE,WONT_DO}
+  enum State {
+    TODO(' '),IN_PROGRESS('.'),DONE('X'),WONT_DO('-');
+    private final char state;
+
+    State(char state) {
+      this.state = state;
+    }
+
+    public char getChar(){
+      return state;
+    }
+  }
 
   public Task(String description) {
     this.description = description;
-    state=State.TODO;
+    state = State.TODO;
   }
 
   @Override
